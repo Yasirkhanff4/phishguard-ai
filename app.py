5d0587201a3f7fab906c86cd8e4cbe756abed278
Yasir Khan <yasirkhany616@gmail.com>
2026-03-30 12:28:54 +0500

    Update app.py

--- app.py
+++ app.py
@@ -197,4 +197,4 @@ if __name__ == "__main__":
     print("  PhishGuard AI — Backend Server")
     print("  http://localhost:5000")
     print("=" * 50)
-    app.run(debug=True, port=5000)
+   app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
